6a961ded0a02741a8a9273585743db2b63c5e335
Kamren Sims <simkamr12@gmail.com>
2024-03-17 18:48:57 -0400

    Latest build.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import About from "./about.js";
